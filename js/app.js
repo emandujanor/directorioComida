0f3681ee4ce6b450f7ac9e0c21b33b217127add9
Edith Mandujano <edithmandujano001@gmail.com>
2017-06-06 13:06:24 -0500

    Ya filtra

--- js/app.js
+++ js/app.js
@@ -1,33 +1,27 @@
-/*inicia mapa*/
+//muestra el mapa
 function mostrarMapa(coordenadas) {
-//var coordenadas = {lat: 19.4176848, lng: -99.16699};
-var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 18,
-  center: coordenadas
-});
-var marker = new google.maps.Marker({
-  position: coordenadas,
-  map: map
-});
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: coordenadas
+  });
+  var marker = new google.maps.Marker({
+    position: coordenadas,
+    map: map
+  });
 }
-
-
-
-
-
-var lugares = [
-	{
-		"nombre": "La Tecla",
-		"lat": "19.4176848",
-		"log": "-99.16699",
+//arreglo con los datos de los lugares que se mostrarán
+var lugares = [{
+    "nombre": "La Tecla",
+    "lat": "19.4176848",
+    "log": "-99.16699",
     "direccion": " Durango 186, Cuauhtémoc, Roma Norte, Roma Nte., 06700 Ciudad de México, CDMX, México ",
-	},
-	{
+  },
+  {
     "nombre": "Barracuda Dinner",
     "lat": "19.4160459",
     "log": "-99.1701702",
     "direccion": " Av Nuevo León 4-A, Condesa, Hipódromo, 06140 Cuauhtémoc, CDMX, México ",
-	},
+  },
   {
     "nombre": "El Manjar",
     "lat": "19.4173654",
@@ -47,69 +41,75 @@ var lugares = [
     "direccion": "Eje 3 Poniente Medellín 188, Roma Norte, Roma Nte., 06700 Cuauthémoc, CDMX, México",
   }
 ];
-
-var plantillaLugares =   '<div class="col s12 m7 lugarNombre" data-longitud="__longitud__" data-latitud="__latitud__">' +
-    '<h2 class="header">__nombre__</h2>' +
-    '<div class="card horizontal">' +
-      '<div class="card-image">' +
-        '<img src="http://via.placeholder.com/120x120">' +
-      '</div>' +
-      '<div class="card-stacked">' +
-        '<div class="card-content">' +
-          '<p>__direccion__</p>' +
-        '</div>' +
-      '</div>' +
-    '</div>' +
+//plantilla con la que se armarán nuestras tarjetas
+var plantillaLugares = '<div class="col s12 m7 lugarNombre" data-longitud="__longitud__" data-latitud="__latitud__">' +
+  '<h2 class="header">__nombre__</h2>' +
+  '<div class="card horizontal">' +
+  '<div class="card-image">' +
+  '<img src="http://via.placeholder.com/120x120">' +
+  '</div>' +
+  '<div class="card-stacked">' +
+  '<div class="card-content">' +
+  '<p>__direccion__</p>' +
+  '</div>' +
+  '</div>' +
+  '</div>' +
   '</div>';
+//muestra las tarjetas que se crearon  en el html
+var mostrarLugares = function(lugares) {
+  var plantillaFinal = "";
+  lugares.forEach(function(lugares) {
+    plantillaFinal += plantillaLugares.replace("__nombre__", lugares.nombre).replace("__direccion__", lugares.direccion)
+      .replace("__latitud__", lugares.lat)
+      .replace("__longitud__", lugares.log)
+      .replace("__direccion__", lugares.direccion);
+  });
+  $("#contenedorTarjetas").html(plantillaFinal);
+};
+//Cambia los datos de latitud y longitud dependiendo del elemento al que se le dio click
+function cambiarUbicacion() {
+  var latitud = $(this).data("latitud");
+  var longitud = $(this).data("longitud");
 
-  var mostrarLugares = function (lugares) {
-  	var plantillaFinal = "";
-  	lugares.forEach(function (lugares) {
-  		plantillaFinal += plantillaLugares.replace("__nombre__", lugares.nombre)  .replace("__direccion__", lugares.direccion)
-        .replace("__latitud__", lugares.lat)
-        .replace("__longitud__", lugares.log)
-  			.replace("__direccion__", lugares.direccion);
-  	});
-  	$("#contenedorTarjetas").html(plantillaFinal);
+  var coordenadas = {
+    lat: latitud,
+    lng: longitud
   };
+  mostrarMapa(coordenadas);
+};
+//verifica si el navegador soporta la api de geolocalización
+function obtenerUbicacionActual() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(mostrarPosicionActual);
+  } else {
+    alert("Geolocalización no es soportado en tu navegador");
+  }
+}
+//muestra el mapa con la ubicación del ordenador
+function mostrarPosicionActual(posicion) {
+  var latitud = posicion.coords.latitude;
+  var longitud = posicion.coords.longitude;
 
-
-  function cambiarUbicacion() {
-    var latitud = $(this).data("latitud");
-    var longitud = $(this).data("longitud");
-
-    var coordenadas = {
-      lat: latitud,
-      lng: longitud
-    };
-    mostrarMapa(coordenadas);
+  var coordenadas = {
+    lat: latitud,
+    lng: longitud
   };
+  mostrarMapa(coordenadas);
+}
 
-  function obtenerUbicacionActual() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(mostrarPosicionActual);
-    } else {
-      alert("Geolocalización no es soportado en tu navegador");
-    }
-  }
+// Funcion que filtra los Lugares
+var filtrarLugares = function(event) {
+  event.preventDefault();
+  var filtro = $("#busqueda").val().toLowerCase();
+  var lugaresFiltrados = lugares.filter(function(lugares) {
+    return lugares.nombre.toLowerCase().indexOf(filtro) >= 0;
+  });
+  mostrarLugares(lugaresFiltrados);
+};
 
-  function mostrarPosicionActual(posicion) {
-    var latitud = posicion.coords.latitude;
-    var longitud = posicion.coords.longitude;
-
-    var coordenadas = {
-      lat: latitud,
-      lng: longitud
-    };
-
-    mostrarMapa(coordenadas);
-  }
-
-
-
-$(document).ready(function(){
+$(document).ready(function() {
   obtenerUbicacionActual();
   mostrarLugares(lugares);
-
+  $("#busqueda-form").submit(filtrarLugares);
   $(".lugarNombre").click(cambiarUbicacion);
 });
